3779e6b8eb1474f515eac8057aed50b8cecde687
Habbes <haby_habbes@outlook.com>
2016-05-21 14:47:24 +0300

    Add voice classes

---
+++ classes.py
@@ -0,0 +1,10 @@
+
+MALE_CHILD = 'MaleChild'
+MALE_TEEN = 'MaleTeen'
+MALE_ADULT = 'MaleAdult'
+MALE_SENIOR = 'MaleSenior'
+
+FEMALE_CHILD = 'FemaleChild'
+FEMALE_TEEN = 'FemaleTeen'
+FEMALE_ADULT = 'FemaleAdult'
+FEMALE_SENIOR = 'FemaleSenior'
