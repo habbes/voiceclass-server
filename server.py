57e63b190304167d9b901921eb3b75b4ed815057
Habbes <haby_habbes@outlook.com>
2016-05-23 00:27:13 +0300

    Update server api urls and input to use form-data

--- server.py
+++ server.py
@@ -1,36 +1,58 @@
 import json
-from flask import Flask
+from flask import Flask, request
 from classifier import Classifier
 
 classifier = Classifier()
 app = Flask(__name__)
 app.debug = True
 
+
+def json_response(resp, status=200):
+    return json.dumps(resp), status, {'Content-Type': 'application/json'}
+
+
+def server_error(message=''):
+    if not message:
+        message = 'Error occured, please try again.'
+    return json_response({'message': message}, 500)
+
 @app.route('/')
 def ping():
     return "OK", 200
 
-@app.route('/api/clips/train')
+@app.route('/api/train', methods=['POST'])
 def train():
-    body = request.get_json()
-    class_name = body['class_name']
+    class_name = request.form('class')
     f = request.files['audio']
     audio = f.read()
-    classifier.classify_audio(audio, class_name)
-    classifier.train()
-    return "0K", 200, 'text/plain'
+    try:
+        classifier.classify_audio(audio, class_name)
+        classifier.train()
+        return json_response({'message': 'OK'})
+    except ValueError as e:
+        return server_error()
 
 
-@app.route('/api/clips/classify', methods=['POST'])
+@app.route('/api/classify', methods=['POST'])
 def classify():
-    return "OK", 200, 'text/plain'
+    f = request.files['audio']
+    audio = f.read()
+    try:
+        res = classifier.detect_class(audio)
+        return json_response(res)
+    except:
+        return server_error()
 
-@app.route('/api/clips/<file_id>/feedback', methods=['POST'])
-def feedback():
-    class_name = request.form['class_name']
-    classifier.classify_unclassified(file_id, class_name)
-    classifier.train()
-    return "0K", 200, 'text/plain'
+
+@app.route('/api/feedback', methods=['POST'])
+def feedback(file_id):
+    class_name = request.form('class')
+    try:
+        classifier.classify_unclassified(file_id, class_name)
+        classifier.train()
+        return json_response({'message':'OK'})
+    except ValueError as e:
+        return server_error()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
